44f7b3f983fc3a7e79bce77edf557eff9c5864db
a23246542 <a23246542@gmail.com>
2021-01-23 20:40:06 +0800

    全域CSS+emotion

---
+++ src/style.js
@@ -0,0 +1,17 @@
+// import { styled } from 'emotion';
+import styled from '@emotion/styled';
+// $ npm install @emotion/react//@@差別
+
+export const Conationer = styled.div`
+  background-color: #ededed;
+  height:100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const WeatherCard = styled.div`
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999;
+  background-color: #f9f9f9;
+`
