0d5db5c2903668d3e272b9a13c001e8d66c4d010
a23246542 <a23246542@gmail.com>
2021-01-24 15:42:25 +0800

    feat: 台灣好天氣UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import {
   Conationer,
-  WeatherCard
+  WeatherCard,
+  Location,
+  Description,
+  CurrentWeather,
+  Temperature,
+  Celsius,
+  WeatherIcon,
+  AirFlow,
+  Rain,
+  Refresh
 } from './style';
+import { ReactComponent as DayCloudyIcom } from './images/day-cloudy.svg';
+import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
+import { ReactComponent as RainIcon } from './images/rain.svg';
+import { ReactComponent as RefreshIcon } from './images/refresh.svg'
 
 function App() {
   return (
     <Conationer>
       <WeatherCard>
-        天氣App
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>23<Celsius>°C</Celsius></Temperature>
+          <DayCloudyIcom/>
+        </CurrentWeather>
+        <AirFlow>
+          <AirFlowIcon/>23 m/h
+        </AirFlow>
+        <Rain>
+          <RainIcon/>48%
+        </Rain>
+        <Refresh>
+          最後觀測時間:上午 12:03<RefreshIcon/>
+        </Refresh>
       </WeatherCard>
     </Conationer>
   );
--- src/style.js
+++ src/style.js
@@ -11,7 +11,69 @@ export const Conationer = styled.div`
 `
 
 export const WeatherCard = styled.div`
+  position: relative;
   min-width: 360px;
   box-shadow: 0 1px 3px 0 #999;
   background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
 `
+
+export const Location  = styled.div`
+  font-size:28px;
+  margin-bottom: 20px;
+  color: #212121;
+`
+export const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`
+
+export const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+export const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+export const WeatherIcon = styled.div``;
+
+export const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+export const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+`;
+
+export const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+`;
+
+export const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #828282;
+`;
